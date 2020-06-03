94959ee1bd9e9d6157f1e4d25cffdeb2cb582325
root <account-login@users.noreply.github.com>
2020-06-03 13:37:41 +0800

    update Makefile.

--- rules.py
+++ rules.py
@@ -31,25 +31,23 @@ def rules(ctx):
         'test_base64.cpp',
     ]
 
+    # all
+    ctx.add_rule('all', ['pty_proxy_master', 'pty_proxy_slave'], ['true'])
+
     # compile objects
     for file in c_files:
         cmd = [CXX, *CXXFLAGS, '-o', o(file), '-c', file, '-MD', '-MP']
         ctx.add_rule(o(file), [file], cmd, d_file=d(file))
 
     # compile binaries
-    all_targets = []
     for exe in ['master', 'slave']:
         exe_file = f'pty_proxy_{exe}'
         o_files = [o(file) for file in lib_files] + [o(f'{exe}.cpp')]
         cmd = [LD, *LD_FLAGS, '-o', exe_file, *o_files]
         ctx.add_rule(exe_file, o_files, cmd)
-        all_targets.append(exe_file)
 
     # tests
     exe_file = 'test_base64'
     o_files = [o('test_base64.cpp'), o('base64.c'), o('doctest.cpp')]
     cmd = [LD, *LD_FLAGS, '-o', exe_file, *o_files]
     ctx.add_rule(exe_file, o_files, cmd)
-
-    # all
-    ctx.add_rule('all', all_targets, ['true'])
